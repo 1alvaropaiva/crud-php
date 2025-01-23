ab19604070871f5b191700b1a88bb668bf3d7590
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-01-23 15:15:33 -0300

    ultimo commit

--- mensagem.php
+++ mensagem.php
@@ -1,16 +1,13 @@
-<?php 
-    if (isset($_SESSION['mensagem'])):
+<?php
+	if (isset($_SESSION['mensagem'])):
 ?>
 
 <div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <?php 
-        echo $_SESSION['mensagem'];
-    ?>
-    <button type="button" aria-label="Close" data-bs-dismiss="alert" class="btn-close">
-    </button>
+	<?= $_SESSION['mensagem']; ?>
+	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 
-<?php 
-    unset($_SESSION['mensagem']);
-    endif;
+<?php
+	unset($_SESSION['mensagem']);
+	endif;
 ?>
